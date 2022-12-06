29a6c204f8c20e141bafef23703111169d7d19e8
Sébastien Piquemal <sebpiq@gmail.com>
2022-12-06 17:36:56 +0100

    restored line deleted by mistake

--- index.ts
+++ index.ts
@@ -2,4 +2,5 @@ import compile from './src/compile'
 export default compile
 export { Code, CompilerSettings } from './src/types'
 export { JavaScriptEngine } from './src/engine-javascript/types'
+export { default as NODE_IMPLEMENTATIONS } from './src/nodes'
 export * as AssemblyscriptWasmBindings from './src/engine-assemblyscript/assemblyscript-wasm-bindings'
