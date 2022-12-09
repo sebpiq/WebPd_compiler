b0ad42d91b61f78ae6e815ff6f6d554b01c1fd40
Sébastien Piquemal <sebpiq@gmail.com>
2022-12-09 10:47:07 +0100

    updated rollup

--- index.ts
+++ index.ts
@@ -1,14 +1,6 @@
-export { default as compile } from './src/compile'
+import { default as compile } from './src/compile'
+export default compile
 export {
     Code,
     CompilerSettings,
-    NodeCodeGenerator,
-    NodeImplementation,
-    NodeImplementations,
 } from './src/types'
-export {
-    MESSAGE_DATUM_TYPE_FLOAT,
-    MESSAGE_DATUM_TYPE_STRING,
-} from './src/constants'
-export * as AssemblyscriptWasmBindings from './src/engine-assemblyscript/assemblyscript-wasm-bindings'
-export * as nodeImplementationsTestHelpers from './src/test-helpers-node-implementations'
