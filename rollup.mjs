133f27cbb935c5cce3b46160cbb0390d96333efe
Sébastien Piquemal <sebpiq@protonmail.com>
2023-12-21 15:51:10 +0100

    started to clean dist build config files

--- rollup.mjs
+++
@@ -1,7 +0,0 @@
-import { buildRollupConfig } from '@webpd/dev/configs/rollup.mjs'
-export default buildRollupConfig({
-    importAsString: [
-        '**/*.asc',
-        './src/core-code/*.js.txt',
-    ],
-})
---
+++ src/index.ts
@@ -0,0 +1,29 @@
+export { default as default } from './compile'
+export {
+    CompilationSettings,
+    Compilation,
+    NodeImplementations,
+    CompilerTarget,
+    AudioSettings,
+} from './compile/types'
+export { Code } from './ast/types'
+export { FS_OPERATION_SUCCESS, FS_OPERATION_FAILURE } from './stdlib/fs'
+export { Engine, Message, FloatArray } from './run/types'
+export { createEngine as createAssemblyScriptWasmEngine } from './engine-assemblyscript/run'
+export { createEngine as createJavaScriptEngine } from './engine-javascript/run'
+export { readMetadata } from './run'
+export { getFloatArrayType } from './run/run-helpers'
+export { createNamespace } from './compile/namespace'
+export * as functional from './functional-helpers'
+export * as dspGraph from './dsp-graph'
+export { DspGraph } from './dsp-graph'
+export * as stdlib from './stdlib'
+export {
+    Var,
+    ConstVar,
+    Func,
+    ast,
+    Class,
+    AnonFunc,
+    Sequence,
+} from './ast/declare'
